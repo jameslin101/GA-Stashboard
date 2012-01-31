6d47446715cce600ca576703df3911488b33ab7b
James Lin <jameslin101@gmail.com>
2012-01-31 10:08:19 -0500

    clearance, page controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Activerecord::Application.config.secret_token = 'edeea3d94d1004d087fcfbe51003af2037ca4f36e12eb31147591b48eb5ed6b1ec0cffb7f7fb78f77ea3ca97b3f72e173e4a31a3ff938be1ee6c93864d9eb255'
