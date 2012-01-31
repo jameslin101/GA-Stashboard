ab621a5275e7332bfc8fd9dacbf8df7cfb1d3caa
James Lin <jameslin101@gmail.com>
2012-01-31 14:19:03 -0500

    printing services

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120131135311) do
+ActiveRecord::Schema.define(:version => 20120131154947) do
+
+  create_table "services", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "services", ["user_id"], :name => "index_services_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email"
